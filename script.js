51bae733ceb3afefc2e7eddb2bd399525f192c81
Louise <louise@louisefindlay.com>
2020-09-20 16:11:21 +0100

    Moved JS to file and added remove list item buttons

---
+++ script.js
@@ -0,0 +1,47 @@
+var shoppingList = new Vue({
+    el: '#shopping-list',
+    data: {
+        state: 'default',
+        header: 'shopping list app',
+        newItem: '',
+        items: [{
+                label: '10 party hats',
+                purchased: false,
+                highPriority: false,
+                    },
+            {
+                label: '2 board games',
+                purchased: true,
+                highPriority: false,
+                    }, {
+                label: '20 cups',
+                purchased: false,
+                highPriority: true,
+                    },
+                ]
+    },
+    computed: {
+        reversedItems() {
+            return this.items.slice(0).reverse();
+        }
+    },
+    methods: {
+        saveItem: function () {
+            this.items.push({
+                label: this.newItem,
+                purchased: false,
+            }, );
+            this.newItem = '';
+        },
+        removeItem: function (item) {
+            this.items.pop(item);
+        },
+        changeState: function (newState) {
+            this.state = newState;
+            this.newItem = '';
+        },
+        togglePurchased: function (item) {
+            item.purchased = !item.purchased;
+        }
+    }
+});
